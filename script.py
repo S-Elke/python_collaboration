144b01ae96a3a5b56f86eee5408235fa2ca5d0ec
S-Elke <elkerton.s@northeastern.edu>
2025-02-02 18:29:42 -0500

    Secondary commit

--- script.py
+++ script.py
@@ -4,18 +4,22 @@ def todo():
     to_do_list = []
     condition = False
     while condition == False:
+        os.system('cls' if os.name == 'nt' else 'clear')
         prompt = input("To add new tasks: type |Add|\nTo view all tasks: type |View|\nTo mark tasks as complete: type |Mark|\nTo delete tasks: type |Delete|\nTo exit: type |Exit|\n")
 
         if prompt == "Add":
             add = input("Enter task name: ")
             to_do_list.append(add + "[]")
             os.system('cls' if os.name == 'nt' else 'clear')
+            
         elif prompt == "View":
             print("\n")
             for i in range(0, len(to_do_list)):
                 print(to_do_list[i])
+            print("\n")
             i = input("Type |con| if you want to continue :")
             os.system('cls' if os.name == 'nt' else 'clear')
+            
         elif prompt == "Mark":
             print("\n")
             for i in range(0, len(to_do_list)):
@@ -24,6 +28,7 @@ def todo():
             current = str(to_do_list[i])
             to_do_list[i] = current[0:(len(current)-2)] + "[\u2713]"
             os.system('cls' if os.name == 'nt' else 'clear')
+            
         elif prompt == "Delete":
             print("\n")
             for i in range(0, len(to_do_list)):
@@ -31,6 +36,7 @@ def todo():
             i = int(input("Type the number of the task you want to delete: "))
             to_do_list.remove(to_do_list[i])
             os.system('cls' if os.name == 'nt' else 'clear')
+            
         elif prompt == "Exit":
             os.system('cls' if os.name == 'nt' else 'clear')
             condition = True
